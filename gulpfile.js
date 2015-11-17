a3e64e32bcdb3227cf9df3509ce6b1d30b9b10d1
ruibin <dev.ruibin.chen@gmail.com>
2015-11-17 10:12:52 +0800

    Add feature to deploy to gh-pages

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync');
+var gp_deploy = require('gulp-gh-pages');
 
 gulp.task('browser-sync', function() {
     browserSync({
@@ -8,6 +9,12 @@ gulp.task('browser-sync', function() {
             baseDir: "."
         }
     });
+});
+
+var options = {};
+gulp.task('deploy', function () {
+  return gulp.src(['./**/*', '!./node_modules/**'])
+  .pipe(gp_deploy(options));
 });
 
 gulp.task('default', ["browser-sync"]);
